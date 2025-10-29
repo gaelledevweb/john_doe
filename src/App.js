6ebe13eb7163922f1f88be82e5cd4b687658748d
GaelleDevWeb72 <gaelle.devweb72@gmail.com>
2025-10-29 16:14:41 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
 import './App.css';
+import NavBar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from "../src/pages/Home";
+import Contact from "../src/pages/Contact";
+import Portfolio from "../src/pages/Portfolio";
+import Services from "../src/pages/Services";
+import MentionsLegales from "../src/pages/MentionsLegales";
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <NavBar />
+
+
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/portfolio' element={<Portfolio />}></Route>
+        <Route path='/services' element={<Services />}></Route>
+        <Route path='/mentionslegales' element={<MentionsLegales />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
+    return (
+        <nav className="navbar">
+            <Link to="/" className="navbar-logo" onClick={closeMenu}>
+                <span>John Doe</span>
+            </Link>
+
+            <div className="menu-icon" onClick={toggleMenu}>
+                <i className={menuOpen ? "bi bi-x-lg" : "bi bi-justify"}></i>
+            </div>
+
+
+            <ul className={menuOpen ? "nav-menu active" : "nav-menu"}>
+                <li className="nav-item">
+                    <Link to="/" className="nav-links" onClick={closeMenu}>Home</Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link to="/contact" className="nav-links" onClick={closeMenu}>Contact</Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link to="/portfolio" className="nav-links" onClick={closeMenu}>Portfolio</Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link to="/services" className="nav-links" onClick={closeMenu}>Services</Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link to="/mentionslegales" className="nav-links" onClick={closeMenu}>Mentions légales</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
